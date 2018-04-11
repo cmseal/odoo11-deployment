8be6213b2ce5c07eda0b128774c1479106c4e149
Chris Seal <chris.seal@me.com>
2018-04-11 12:36:30 +0100

    Tidy up

--- odoo11-deployment.sh
+++ odoo11-deployment.sh
@@ -9,11 +9,11 @@
 ################################################
 
 #You can install Odoo 10, if you wish...
-OE_VERSION="$11"
+ODOO_VERSION="$11"
 
-if [$OE_VERSION != 10 && $OE_VERSION != 11 ]; then
-	echo "Odoo version is invalid, please enter either 10 or 11"
-	exit 1
+if [$ODOO_VERSION != 10 && $ODOO_VERSION != 11 ]; then
+  echo "Odoo version is invalid, please enter either 10 or 11"
+  exit 1
 fi
 
 URL=$1
@@ -23,51 +23,38 @@ if [ "$URL" = "" ]; then
   exit 1
 fi
 
+ODOO_USER="odoo"
+USER_HOME="/home/$ODOO_USER"
+INSTALL="/home/$ODOO_USER/odoo"
+ODOO_PORT="8069"
+ODOO_ADMIN="admin"
+ODOO_CONFIG="$ODOO_USER"
 
-##fixed parameters
-OE_USER="odoo"
-OE_HOME="/home/$OE_USER"
-OE_HOME_EXT="/home/$OE_USER/$OE_USER"
-#Set the default Odoo port (you still have to use -c /etc/odoo.conf for example to use this.)
-OE_PORT="8069"
-#set the superadmin password
-OE_SUPERADMIN="admin"
-OE_CONFIG="$OE_USER"
-
-echo -e "\n---- Set Hostname ----"
+# Set Hostname
 sudo hostnamectl set-hostname $URL
 sudo su - echo "$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4) $(hostname -f) $(hostname -s)" >> /etc/hosts
 
-echo -e "\n---- Update Server ----"
+# Update/upgrade setver
 sudo apt-get update
 sudo apt-get upgrade -y
 
-echo -e "\n---- Install PostgreSQL Server ----"
+# Install PostgreSQL server
 sudo apt-get install postgresql -y
 
-echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
+# Create Odoo Postgres user
+sudo su - postgres -c "createuser -s $ODOO_USER" 2> /dev/null || true
 
-echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install python3 python3-pip -y
+# Install Python3, Pip3 and tools
+sudo apt-get install python3 python3-pip wget git bzr python-pip gdebi-core -y
 
-echo -e "\n---- Install tool packages ----"
-sudo apt-get install wget git bzr python-pip gdebi-core -y
-
-echo -e "\n---- Install python packages ----"
-sudo apt-get install python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil python-babel python-paramiko python-psycogreen python-ply -y
-
+# Install Python packages
+sudo apt-get install python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil python-babel python-paramiko python-psycogreen python-ply python3-suds -y
 sudo pip3 install pypdf2 Babel passlib Werkzeug decorator python-dateutil pyyaml psycopg2 psutil html2text docutils lxml pillow reportlab ninja2 requests gdata XlsxWriter vobject python-openid pyparsing pydot mock mako Jinja2 ebaysdk feedparser xlwt psycogreen suds-jurko pytz pyusb greenlet xlrd
 
-echo -e "\n---- Install python libraries ----"
-sudo apt-get install python3-suds -y
+# Install other packages
+sudo apt-get install node-clean-css node-less python-gevent -y
 
-echo -e "\n--- Install other required packages ----"
-sudo apt-get install node-clean-css -y
-sudo apt-get install node-less -y
-sudo apt-get install python-gevent -y
-
-echo -e "\n--- Install and configure apache ----"
+# Install Apache, PHP and enable mods
 sudo apt-get install apache2 php7.0 libapache2-mod-php7.0 php7.0-xmlrpc xfonts-75dpi -y
 sudo /etc/init.d/apache2 start
 sudo update-rc.d apache2 defaults
@@ -89,24 +76,24 @@ sudo chown -R odoo:root /var/log/odoo
 sudo chown -R odoo:adm /var/log/apache2
 sudo service apache2 reload
 
-echo -e "\n---- Install wkhtml and place shortcuts on correct place for ODOO ----"
+# Install wkhtmltopdf and shortcuts
 sudo wget https://downloads.wkhtmltopdf.org/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo gdebi --n wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
 sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
 
-echo -e "\n---- Create ODOO user ----"
-sudo adduser $OE_USER --quiet --disabled-password --gecos ""
-sudo adduser $OE_USER sudo
-sudo echo "$OE_USER ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
+# Create Odoo user and setup sudo
+sudo adduser $ODOO_USER --quiet --disabled-password --gecos ""
+sudo adduser $ODOO_USER sudo
+sudo echo "$ODOO_USER ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
 
-echo -e "\n---- Create Log directory ----"
-sudo mkdir /var/log/$OE_USER
-sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
+# Create log folder
+sudo mkdir /var/log/$ODOO_USER
+sudo chown $ODOO_USER:$ODOO_USER /var/log/$ODOO_USER
 
-echo -e "\n==== Installing ODOO Server ===="
-if [$OE_VERSION == 10]; then
-  if ! sudo git clone https://github.com/mcb30/odoo.git --depth 20 --branch import $OE_HOME_EXT/
+# Install Odoo
+if [$ODOO_VERSION == 10]; then
+  if ! sudo git clone https://www.github.com/odoo/odoo --depth 1 --branch 11.0 $INSTALL/
    then
     echo >&2
     echo "Deployment halted here.  Unable to clone Odoo.";
@@ -114,34 +101,35 @@ if [$OE_VERSION == 10]; then
   fi
   sudo mkdir -p ~/.local/share/Odoo/addons/10.0
 else
-	if ! sudo git clone https://www.github.com/odoo/odoo --depth 1 --branch 11.0 $OE_HOME_EXT/
-		then
-		echo >&2
-		echo "Deployment halted here.  Unable to clone Odoo.";
+  if ! sudo git clone https://www.github.com/odoo/odoo --depth 1 --branch 11.0 $INSTALL/
+    then
+    echo >&2
+    echo "Deployment halted here.  Unable to clone Odoo.";
     exit 1
   fi
-	sudo mkdir -p ~/.local/share/Odoo/addons/11.0
+  sudo mkdir -p ~/.local/share/Odoo/addons/11.0
 fi
+sudo chown -R $ODOO_USER:$ODOO_USER $USER_HOME/*
 
-echo -e "\n---- Setting permissions on home folder ----"
-sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
+# Create Odoo conf file
+cat <<EOF > ~/$ODOO_CONFIG.conf
+[options]
+This is the password that allows database operations:
+admin_passwd = ${ODOO_ADMIN}
+xmlrpc_port = ${ODOO_PORT}
+logfile = /var/log/odoo/odoo.log
+EOF
 
-echo -e "* Create server config file"
-sudo touch /etc/$OE_CONFIG.conf
+# Install Odoo conf file
+sudo mv ~/$ODOO_CONFIG.conf /etc/$ODOO_CONFIG.conf
+sudo chown $ODOO_USER:$ODOO_USER /etc/$ODOO_CONFIG.conf
+sudo chmod 640 /etc/$ODOO_CONFIG.conf
 
-echo -e "* Creating server config file"
-sudo su root -c "printf '[options] \n; This is the password that allows database operations:\n' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "printf 'admin_passwd = ${OE_SUPERADMIN}\n' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "printf 'xmlrpc_port = ${OE_PORT}\n' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "printf 'logfile = /var/log/odoo/odoo.log\n' >> /etc/$OE_CONFIG.conf"
-sudo chown $OE_USER:$OE_USER /etc/$OE_CONFIG.conf
-sudo chmod 640 /etc/$OE_CONFIG.conf
-
-echo -e "* Create init file"
-cat <<EOF > ~/$OE_CONFIG
+# Create Odoo service file
+cat <<EOF > ~/$ODOO_CONFIG
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides: $OE_CONFIG
+# Provides: $ODOO_CONFIG
 # Required-Start: \$remote_fs \$syslog
 # Required-Stop: \$remote_fs \$syslog
 # Should-Start: \$network
@@ -152,13 +140,13 @@ cat <<EOF > ~/$OE_CONFIG
 # Description: ODOO Business Applications
 ### END INIT INFO
 PATH=/bin:/sbin:/usr/bin
-DAEMON=$OE_HOME_EXT/odoo-bin
-NAME=$OE_CONFIG
-DESC=$OE_CONFIG
+DAEMON=$INSTALL/odoo-bin
+NAME=$ODOO_CONFIG
+DESC=$ODOO_CONFIG
 # Specify the user name (Default: odoo).
-USER=$OE_USER
+USER=$ODOO_USER
 # Specify an alternate config file (Default: /etc/odoo.conf).
-CONFIGFILE="/etc/$OE_CONFIG.conf"
+CONFIGFILE="/etc/$ODOO_CONFIG.conf"
 # pidfile
 PIDFILE=/var/run/\${NAME}.pid
 # Additional options that are passed to the Daemon.
@@ -204,23 +192,22 @@ esac
 exit 0
 EOF
 
-echo -e "* Security Init File"
-sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
-sudo chmod 755 /etc/init.d/$OE_CONFIG
-sudo chown root: /etc/init.d/$OE_CONFIG
+# Install Odoo service file
+sudo mv ~/$ODOO_CONFIG /etc/init.d/$ODOO_CONFIG
+sudo chmod 755 /etc/init.d/$ODOO_CONFIG
+sudo chown root: /etc/init.d/$ODOO_CONFIG
 
-echo -e "* Start ODOO on Startup"
-sudo update-rc.d $OE_CONFIG defaults
+# Set Odoo service to start on boot
+sudo update-rc.d $ODOO_CONFIG defaults
 
-echo -e "\n--- Create DocumentRoot ----"
+# Make vhost document root
 sudo mkdir -p /var/www/vhosts/$(hostname)/httpdocs
 
-echo -e "\n--- Create conf file ----"
+# Create Apache conf file
 sudo cat <<EOF > ~/$(hostname).conf
 <Virtualhost *:80>
       ServerAdmin webmaster@localhost
       ServerName $(hostname)
-
       DocumentRoot /var/www/vhosts/$(hostname)/httpdocs
       ErrorLog ${APACHE_LOG_DIR}/$(hostname)_error.log
       CustomLog ${APACHE_LOG_DIR}/$(hostname)_access.log combined
@@ -239,18 +226,17 @@ sudo cat <<EOF > ~/$(hostname).conf
             AllowOverride All
             Require all granted
       </Directory>
-      
 </Virtualhost>
 EOF
 
-echo -e "\n--- Enable site and reload Apache ----"
+# Enable site and reload Apache
 sudo mv ~/$(hostname).conf /etc/apache2/sites-available/
 sudo a2ensite $(hostname).conf
 sudo service apache2 reload
 
 echo "-----------------------------------------------------------"
 echo "Done! The Odoo server is up and running."
-echo "Start Odoo service: sudo service $OE_CONFIG start"
-echo "Stop Odoo service: sudo service $OE_CONFIG stop"
-echo "Restart Odoo service: sudo service $OE_CONFIG restart"
+echo "Start Odoo service: sudo service $ODOO_CONFIG start"
+echo "Stop Odoo service: sudo service $ODOO_CONFIG stop"
+echo "Restart Odoo service: sudo service $ODOO_CONFIG restart"
 echo "-----------------------------------------------------------"
