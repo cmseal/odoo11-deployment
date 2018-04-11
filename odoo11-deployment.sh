3b815e9d4a17374aaeaaa8b2e80e1f203786cfa6
Chris Seal <chris.seal@me.com>
2018-04-11 14:03:31 +0100

    Update odoo11-deployment.sh

--- odoo11-deployment.sh
+++ odoo11-deployment.sh
@@ -11,7 +11,7 @@
 #You can install Odoo 10, if you wish...
 ODOO_VERSION="11"
 
-if [$ODOO_VERSION != 10 && $ODOO_VERSION != 11 ]; then
+if [$ODOO_VERSION != "10" && $ODOO_VERSION != "11" ]; then
   echo "Odoo version is invalid, please enter either 10 or 11"
   exit 1
 fi
